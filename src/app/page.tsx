9a884157f83aaa2aa81e4e0bc8deda9980696f7e
Emily Berger <bergeremily2@gmail.com>
2023-11-09 21:29:38 -0500

    fixed spacing on title for mobiles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,10 +13,10 @@ export default function Home() {
                     </h1>
                 </div>
                 <div className="w-full pt-4 h-screen max-h-screen col-span-5 sm:col-span-2 lg:col-span-1" style={{backgroundColor:''}}>
-                    <h1 className="text-4xl text-justify mb-4 sm:hidden font-extrabold leading-none tracking-tight text-indigo-950 md:text-5xl lg:text-6xl dark:text-indigo-200">
+                    <h1 className="mx-4 text-4xl text-justify mb-4 sm:hidden font-extrabold leading-none tracking-tight text-indigo-950 md:text-5xl lg:text-6xl dark:text-indigo-200">
                         HELLO, MY NAME IS EMILY BERGER
                     </h1>
-                    <div className="grid-container grid grid-cols-1 gap-3 justify-items-end mr-4 xxs:grid-cols-1 sm:justify-items-end sm:grid-cols-1 sm:gap-3">
+                    <div className="grid-container grid grid-cols-1 gap-3 justify-items-end mr-4 xxs:grid-cols-4 xxs:justify-items-center sm:justify-items-end sm:grid-cols-1 sm:gap-3">
                         <Slide title="HOME" link="/" current="HOME"/>
                         <Slide title="PROJECTS" link="/projects" current="HOME"/>
                         <Slide title="RESUME" link="/resume" current="HOME"/>
--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -13,7 +13,7 @@ export default function Resume() {
             </h1>
         </div>
         <div className="w-full pt-4 h-screen max-h-screen col-span-5 sm:col-span-2 lg:col-span-1" style={{backgroundColor:''}}>
-          <h1 className="text-4xl text-justify mb-4 sm:hidden font-extrabold leading-none tracking-tight text-indigo-950 md:text-5xl lg:text-6xl dark:text-indigo-200">
+          <h1 className="mx-4 text-4xl text-justify mb-4 sm:hidden font-extrabold leading-none tracking-tight text-indigo-950 md:text-5xl lg:text-6xl dark:text-indigo-200">
             HELLO, MY NAME IS EMILY BERGER
           </h1>
           <div className="grid-container grid grid-cols-1 gap-3 justify-items-end mr-4 xs:mr-0 xxs:grid-cols-4 xxs:justify-items-center sm:justify-items-end sm:grid-cols-1 sm:gap-3">

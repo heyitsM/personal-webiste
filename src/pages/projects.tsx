a36987420c758183be62b7cb5425ffb1e1c59ea6
Emily Berger <bergeremily2@gmail.com>
2023-12-26 02:17:12 -0600

    fixing typos

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -12,7 +12,7 @@ export default function Projects() {
                         <div className="grid grid-cols-2 lg:grid-cols-3 gap-5 mr-8 pt-4">
                             <Project title="Personal Website" image="Personal-Webiste.png" link="https://emily-heyitsm.vercel.app" github="https://github.com/heyitsM/personal-webiste" description="This is my current personal website." />
                             <Project title="Personal Website - Old" image="heyitsm.onrender.com.png" link="https://heyitsm.onrender.com" github="https://github.com/heyitsM/heyitsM.github.io" description="A previous and unfinished personal website." />
-                            <Project title="EAR Chrome Extension" image="ear-detection.png" link="https://github.com/Kennnnn774/EAR-Detection-Tool" github="https://github.com/Kennnnn774/EAR-Detection-Tool" description="A project designed to detect execute-after-redirect vulnerabilities using dynamic analysis." />
+                            <Project title="EAR Chrome Extension" image="ear-detection.png" link="https://github.com/Kennnnn774/EAR-Detection-Tool" github="https://github.com/Kennnnn774/EAR-Detection-Tool" description="A tool designed to detect execute-after-redirect vulnerabilities using dynamic analysis." />
                             <Project title="Personal Website" image="Personal-Webiste.png" link="" github="" description="description goes here" />
                         </div>
                         <h1 className="text-4xl place-self-end justify-self-start font-extrabold leading-none tracking-tight text-indigo-950 md:text-5xl lg:text-6xl dark:text-indigo-200">

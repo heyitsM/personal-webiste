1eedab88b23ffe090f2822871c2e91e5a6111fd0
Emily Berger <bergeremily2@gmail.com>
2023-12-26 01:45:58 -0600

    resume updates

---
+++ src/components/resume_body.tsx
@@ -0,0 +1,17 @@
+export default function ResumeBody() {
+    return (
+        <div className="text-l text-indigo-800 dark:text-indigo-500 rounded-lg p-4 h-fit border-2 border-indigo-900/50 hover:border-indigo-900/20 backdrop-blur-sm bg-gradient-to-b from-indigo-950/20 via-violet-950/20 to-purple-950/20 dark:hover:from-purple-950/50 dark:hover:via-violet-950/50 dark:hover:to-indigo-950/50 hover:from-purple-950/10 hover:via-violet-950/10 hover:to-indigo-950/10" >
+            I'm a passionate and aspiring softare engineer, in the process of completing a B.S. in Computer Science at 
+            Johns Hopkins University. I'm most interested in developing full-stack websites, with prior experience in 
+            mostly backend and some frontend development. With knowledge of backend frameworks such as Node/Express, 
+            Flask, and a bit of Django, and a drive to learn more, I am excited to tackle new problems. Check out  
+            <a href="https://emily-heyitsm.vercel.app/projects" className="hover:text-violet-800 dark:hover:text-violet-600"><b> my projects</b></a> to see examples of my work on various 
+            backend projects, and some of my frontend development projects (including this one).
+            <br />
+            <br />
+            I have development experience, both through school, an internship (learn more about my experience with SEO Careers here)
+            and through side projects including this one. To learn more about specifics regarding my school/work experience, please don't 
+            hesitate to contact me!
+        </div>
+    );
+}
--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Slide from '@/components/slide'
 import Layout from '@/components/layout'
 import ResumeComponent from '@/components/resume_component'
+import ResumeBody from '@/components/resume_body'
 
 import '@/app/globals.css'
 
@@ -11,12 +12,13 @@ export default function Resume() {
       <div className="grid-container max-h-screen grid grid-cols-5 gap-7">
         <div className="w-full pl-4 mb-4 place-self-end hidden col-span-0 sm:flex sm:col-span-3 lg:col-span-4" style={{backgroundColor: ''}}>
             <div className="grid-container grid grid-cols-1 gap-8">
-              <div className="w-full grid-container grid grid-cols-2 gap-4">
+              {/* <div className="w-full grid-container grid grid-cols-2 gap-4">
                 <ResumeComponent title="EDUCATION" body="I am studying Computer Science at Johns Hopkins University, and am graduating in May of 2024. I concentrated on software development- software, testing, and team projects, along with web security and other associated topics. I also took a multitude of classes in a variety of other subject areas, including math, neuroscience, and psychology. Click below for a more in-depth exploration of my educational experience!" link=""/>
                 <ResumeComponent title="WORK EXPERIENCE" body="I completed one internship at SEO Tech Developer (Summer 2022) and worked on various projects (both personal and in classes/independent study projects). During High School, I worked as a professionally certified ski instructor and learned many valuable skills (feel free to email me to learn more). Click below for more information on projects I've worked on!" link="https://github.com/heyitsM"/>
                 <ResumeComponent title="CLUBS/INVOLVEMENT" body="I was involved in multiple clubs including, toward the beginning, Jail Tutorial Project (a club involved in advocacy and tutoring individuals who are/have interacted with the prison system), and have also been involved with WiCS (Women in Computer Science) and ACM (Association of Computing Machinery). I was also a member of the Ski Club, and when I have free time, I enjoy running and enjoy discovering new music." link=""/>
                 <ResumeComponent title="INTERESTS" body="I am interested in digging deeper into full-stack development, having experience with developing frontend projects (this website for example) and having experience with backend development as well. I've worked to develop applications in both Node/ExpressJS tech stacks and in Python/Flask stacks, and have some familiarity with Django. I would be very excited to get more experience in any of these domains." link=""/>
-              </div>
+              </div> */}
+              <ResumeBody />
               <h1 className="w-full text-4xl font-extrabold leading-none tracking-tight text-indigo-950 md:text-5xl lg:text-6xl dark:text-indigo-200">
                 HELLO, MY NAME IS EMILY BERGER
               </h1>

a9e53a44d726a0bbb6aa1103df815e5ac15dbd24
Emily Berger <76461173+heyitsM@users.noreply.github.com>
2026-09-03 19:11:21 -0500

    Update projects.tsx

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -10,11 +10,10 @@ export default function Projects() {
                 <div className="w-11/12 max-h-full pl-4 pb-4 hidden col-span-0 sm:flex sm:col-span-3 lg:col-span-4" style={{backgroundColor: ''}}>
                     <div className="grid w-full grid-cols-1">
                         <div className="grid grid-cols-2 lg:grid-cols-3 gap-5 mr-8 pt-4">
-                            <Project title="Personal Website" image="Personal-Webiste.png" link="https://emily-heyitsm.vercel.app" github="https://github.com/heyitsM/personal-webiste" description="This is my current personal website." />
-                            <Project title="Personal Website - Old" image="heyitsm.onrender.com.png" link="https://heyitsm.onrender.com" github="https://github.com/heyitsM/heyitsM.github.io" description="A previous and unfinished personal website." />
-                            <Project title="EAR Chrome Extension" image="ear-detection.png" link="https://github.com/Kennnnn774/EAR-Detection-Tool" github="https://github.com/Kennnnn774/EAR-Detection-Tool" description="A tool to detect execute-after-redirect vulnerabilities with dynamic analysis." />
                             <Project title="Kinematics & Math Simulator" image="kinematics-simulator.onrender.com.png" link="https://kinematics-simulator.onrender.com" github="https://github.com/heyitsM/kinematics-simulator" description="A fun callback to my AP Physics days. This is a revival of one of my first side projects." />
-                            
+                            <Project title="Personal Website" image="Personal-Webiste.png" link="https://emily-heyitsm.vercel.app" github="https://github.com/heyitsM/personal-webiste" description="This is my current personal website." />
+                            <Project title="EAR Chrome Extension" image="ear-detection.png" link="https://github.com/Kennnnn774/EAR-Detection-Tool" github="https://github.com/Kennnnn774/EAR-Detection-Tool" description="A tool to detect execute-after-redirect vulnerabilities with dynamic analysis." />
+                            <Project title="Personal Website - Old" image="heyitsm.onrender.com.png" link="https://heyitsm.onrender.com" github="https://github.com/heyitsM/heyitsM.github.io" description="A previous and unfinished personal website." />
                         </div>
                         <h1 className="text-4xl place-self-end justify-self-start font-extrabold leading-none tracking-tight text-indigo-950 md:text-5xl lg:text-6xl dark:text-indigo-200">
                             HELLO, MY NAME IS EMILY BERGER
@@ -32,9 +31,10 @@ export default function Projects() {
                         <Slide title="CONTACT" link="/contact" current="PROJECTS" />
                     </div>
                     <div className="grid grid-cols-1 xs:grid-cols-2 sm:hidden gap-5 mx-4 pt-4">
+                        <Project title="Kinematics & Math Simulator" image="kinematics-simulator.onrender.com.png" link="https://kinematics-simulator.onrender.com" github="https://github.com/heyitsM/kinematics-simulator" description="A fun callback to my AP Physics days. This is a revival of one of my first side projects." />
                         <Project title="Personal Website" image="Personal-Webiste.png" link="https://emily-heyitsm.vercel.app" github="https://github.com/heyitsM/personal-webiste" description="This is my current personal website." />
+                        <Project title="EAR Chrome Extension" image="ear-detection.png" link="https://github.com/Kennnnn774/EAR-Detection-Tool" github="https://github.com/Kennnnn774/EAR-Detection-Tool" description="A tool to detect execute-after-redirect vulnerabilities with dynamic analysis." />
                         <Project title="Personal Website - Old" image="heyitsm.onrender.com.png" link="https://heyitsm.onrender.com" github="https://github.com/heyitsM/heyitsM.github.io" description="A previous and unfinished personal website." />
-                        <Project title="Personal Website" image="Personal-Webiste.png" link="" github="" description="description goes here" />
                         <Project title="Personal Website" image="Personal-Webiste.png" link="" github="" description="description goes here" />
                     </div>
 
